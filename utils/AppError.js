a403e921b8658305a5d1964764e0cf8aac6a3169
AbirAzim <azim35-1713@diu.edu.bd>
2022-01-18 14:56:40 +0600

    there

--- utils/AppError.js
+++ utils/AppError.js
@@ -10,6 +10,7 @@ class AppError extends Error {
         this.statusCode = statusCode;
         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
         this.isOperational = true;
+        console.log('h1');
         Error.captureStackTrace(this, this.constructor);
     }
 }
