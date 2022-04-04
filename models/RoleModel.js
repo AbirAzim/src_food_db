c4102e63e84e448177f06debb732f1cdb57beae9
AbirAzim <azim35-1713@diu.edu.bd>
2022-04-04 13:58:04 +0600

    there

--- models/RoleModel.js
+++ models/RoleModel.js
@@ -1,120 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
+let roleType = {
+    delete: {
+        type: Boolean,
+        default: false,
+    },
+    create: {
+        type: Boolean,
+        default: false,
+    },
+    edit: {
+        type: Boolean,
+        default: false,
+    },
+    view: {
+        type: Boolean,
+        default: false,
+    },
+};
 const roleSchema = new mongoose_1.Schema({
     roleName: {
         type: String,
         required: [true, 'rolename is requires'],
         unique: true,
     },
-    Users: {
-        delete: {
-            type: Boolean,
-            default: false,
-        },
-        create: {
-            type: Boolean,
-            default: false,
-        },
-        edit: {
-            type: Boolean,
-            default: false,
-        },
-        view: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    Shop: {
-        delete: {
-            type: Boolean,
-            default: false,
-        },
-        create: {
-            type: Boolean,
-            default: false,
-        },
-        edit: {
-            type: Boolean,
-            default: false,
-        },
-        view: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    Dashboard: {
-        delete: {
-            type: Boolean,
-            default: false,
-        },
-        create: {
-            type: Boolean,
-            default: false,
-        },
-        edit: {
-            type: Boolean,
-            default: false,
-        },
-        view: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    Blend: {
-        delete: {
-            type: Boolean,
-            default: false,
-        },
-        create: {
-            type: Boolean,
-            default: false,
-        },
-        edit: {
-            type: Boolean,
-            default: false,
-        },
-        view: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    Admin: {
-        delete: {
-            type: Boolean,
-            default: false,
-        },
-        create: {
-            type: Boolean,
-            default: false,
-        },
-        edit: {
-            type: Boolean,
-            default: false,
-        },
-        view: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    All: {
-        delete: {
-            type: Boolean,
-            default: false,
-        },
-        create: {
-            type: Boolean,
-            default: false,
-        },
-        edit: {
-            type: Boolean,
-            default: false,
-        },
-        view: {
-            type: Boolean,
-            default: false,
-        },
-    },
+    Users: roleType,
+    Shop: roleType,
+    Dashboard: roleType,
+    Blend: roleType,
+    Admin: roleType,
+    All: roleType,
+    Wiki: roleType,
     createdAt: { type: Date, default: Date.now },
 });
 const role = (0, mongoose_1.model)('Role', roleSchema);
